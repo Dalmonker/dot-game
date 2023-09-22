6b4f0aa2817c5575fc0876cd891dd2e7c42ec336
Dalmonker <gavrilovichdmitry1@gmail.com>
2023-09-22 23:30:26 +0300

    createAppleFunc

--- scripts.js
+++ scripts.js
@@ -2,6 +2,10 @@ const dot1 = document.getElementById("dot1");
 const dot2 = document.getElementById("dot2");
 const display = document.getElementById("display");
 
+const block = document.getElementById("block");
+
+const btn = document.getElementById("btn");
+
 let dot1Top = 0;
 let dot2Top = 0;
 
@@ -9,7 +13,6 @@ let dot1Left = 0;
 let dot2Right = 0;
 
 document.addEventListener("keyup", (e) => {
-  console.log(e);
   switch (e.key) {
     // one player
     case "ArrowUp":
@@ -54,3 +57,23 @@ document.addEventListener("keyup", (e) => {
       break;
   }
 });
+
+const Apple = () => {
+  const apple = document.createElement("div");
+  apple.style.width = "50px";
+  apple.style.height = "50px";
+  apple.style.border = "3px solid red";
+
+  const displayWidth = display.clientWidth;
+  const displayHeight = display.clientHeight;
+
+  const randomLeft = Math.random() * displayWidth;
+  const randomTop = Math.random() * displayHeight;
+
+  apple.style.left = randomLeft + "px";
+  apple.style.top = randomTop + "px";
+
+  block.appendChild(apple);
+};
+
+btn.addEventListener("click", Apple);
