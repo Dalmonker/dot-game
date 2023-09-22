edbb17dc99c91bdbf5accb9dfdd9e627710f44d6
Dalmonker <gavrilovichdmitry1@gmail.com>
2023-09-22 21:43:03 +0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,56 @@
+const dot1 = document.getElementById("dot1");
+const dot2 = document.getElementById("dot2");
+const display = document.getElementById("display");
+
+let dot1Top = 0;
+let dot2Top = 0;
+
+let dot1Left = 0;
+let dot2Left = 0;
+
+document.addEventListener("keyup", (e) => {
+  console.log(e);
+  switch (e.key) {
+    // one player
+    case "ArrowUp":
+      dot1Top -= 5;
+      dot1.style.top = dot1Top + "px";
+      break;
+
+    case "ArrowDown":
+      dot1Top += 5;
+      dot1.style.top = dot1Top + "px";
+      break;
+
+    case "ArrowLeft":
+      dot1Left -= 5;
+      dot1.style.left = dot1Left + "px";
+      break;
+
+    case "ArrowRight":
+      dot1Left += 5;
+      dot1.style.left = dot1Left + "px";
+      break;
+
+    // two player
+    case "w":
+      dot2Top -= 5;
+      dot2.style.top = dot2Top + "px";
+      break;
+
+    case "s":
+      dot2Top += 5;
+      dot2.style.top = dot2Top + "px";
+      break;
+
+    case "d":
+      dot2Left -= 5;
+      dot2.style.left = dot2Left + "px";
+      break;
+
+    case "a":
+      dot2Left += 5;
+      dot2.style.left = dot2Left + "px";
+      break;
+  }
+});
